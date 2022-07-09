6f5e8c38c59e491f2f6b3e5639bc9bf8f417cb64
Andrii Dovhaliuk <andy87@live.ru>
2022-07-09 18:51:10 +0400

    Add populateDisplay function

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,46 @@
+let display_value = 0;
+let lastValue = '';
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', populateDisplay);
+});
+
+function populateDisplay(event) {
+    const operations = ['+', '-', '/', '*'];
+    let value = event.target.innerText;
+    if (value === "=") {
+        calculate(display_value);
+    } else if (value === "C") {
+        display_value = 0;
+    } else if (operations.includes(value)) {
+        if (!display_value) {
+            return;
+        } else if (lastValue === 'operator') {
+            display_value = display_value.slice(0,-1) + value;
+        } else {
+            display_value += value;
+        }
+        lastValue = "operator";
+    } else if (display_value) {
+        display_value += value;
+    } else {
+        display_value = value;
+    }
+    display.textContent = display_value;
+}
+
+function calculate(value) {
+    let values = value.split(operationsRegExp);
+    console.log(values);
+}
+
 function add(a , b) {
     return a + b;
 }
 
 function subtract(a, b) {
-    return a -b;
+    return a - b;
 }
 
 function multiply(a , b) {
@@ -14,6 +51,11 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(func, a, b) {
-     return func(a, b);
+function operate(operation, a, b) {
+     switch(operation) {
+        case "+": return add(a,b);
+        case "-": return subtract(a,b);
+        case "*": return multiply(a,b);
+        case "/": return divide(a/b);
+     }
 }
